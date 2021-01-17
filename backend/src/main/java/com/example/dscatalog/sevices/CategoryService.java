159d77af50cb6a45fb8e9beaf104ebe0e3a46a1b
rpźbass <ronaldop.tech@gmail.com>
2021-01-17 11:58:27 -0300

    Insert Category

--- backend/src/main/java/com/example/dscatalog/sevices/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/sevices/CategoryService.java
@@ -40,6 +40,16 @@ public class CategoryService {
 			return  new CategoryDTO(entity);
 		    
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = respository.save(entity);
+		return new CategoryDTO(entity);
+		
+	}
 	
 	
 	
