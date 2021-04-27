b36037b323720576b7ce0c898e0769fcfde67879
rpźbass <ronaldop.tech@gmail.com>
2021-04-27 11:17:46 -0300

    External configuration

--- backend/src/main/java/com/example/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/example/dscatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,73 @@
 package com.example.dscatalog.config;
 
-public class AuthorizationServerConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+@Configuration
+@EnableAuthorizationServer   //anotação que referencia ao spring a classe responsavel pelo oauth ou autorização.
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
+	
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncode;   //criptografa a senha do digitada 
+	
+	@Autowired 
+	private JwtAccessTokenConverter accessTokenConverter; // Converte os tokens 
+	
+	@Autowired
+	private JwtTokenStore tokenStore; //traduz os tokens usando uma palavra passe
+	@Autowired 
+	private AuthenticationManager authenticationManager;
+	
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permiAll()").checkTokenAccess("isAuthenticated()");  //permite acesso  e verifica se o usuario esta Autenticado
+		super.configure(security);
+	}
 
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		.withClient(clientId)
+		.secret(passwordEncode.encode(clientSecret))
+		.scopes("read","write")
+		.authorizedGrantTypes("password")
+		.accessTokenValiditySeconds(jwtDuration);
+		
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+	
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore)
+		.accessTokenConverter(accessTokenConverter);
+	
+	}	
+
+	
+		
+	
+	
+	
 }
